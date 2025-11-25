7ffb925d587d177a312180ad223df1c3effa909f
Alejandro <rodriguez.arale24@triana.salesianos.edu>
2025-11-25 11:48:31 +0100

    nuevo

---
+++ src/test/java/com/salesianostriana/dam/proyectoAlejandroRecetas/ProyectoAlejandroRecetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.proyectoAlejandroRecetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoAlejandroRecetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
