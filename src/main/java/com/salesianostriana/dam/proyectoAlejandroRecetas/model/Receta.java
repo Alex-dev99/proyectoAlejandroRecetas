0faec25d60edeea4c802e15d18dbc63a83d3f3ad
Alejandro <rodriguez.arale24@triana.salesianos.edu>
2025-11-26 13:36:31 +0100

    mejorados modelos

--- src/main/java/com/salesianostriana/dam/proyectoAlejandroRecetas/model/Receta.java
+++ src/main/java/com/salesianostriana/dam/proyectoAlejandroRecetas/model/Receta.java
@@ -1,10 +1,17 @@
 package com.salesianostriana.dam.proyectoAlejandroRecetas.model;
+import jakarta.persistence.*;
 
+@Entity
 public class Receta {
 
+    @Id
+    @GeneratedValue
     long id;
+
     String nombre;
+
     Integer tiempoDePreparacion;
+
     Enum dificultad;
     
 }
